fc26c93261ae1014b92e3b1ba9af9e5c74de0733
h4dh4d1y4551n3 <hadhadiyassine@gmail.com>
2024-08-23 00:57:23 +0100

    Refactor lexer interface

--- lexer.h
+++ lexer.h
@@ -6,7 +6,7 @@
 /*   By: yhadhadi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 03:07:15 by yhadhadi          #+#    #+#             */
-/*   Updated: 2024/08/22 06:13:01 by yhadhadi         ###   ########.fr       */
+/*   Updated: 2024/08/23 00:56:28 by yhadhadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,54 +17,61 @@
 
 enum e_tok
 {
-	TOK_EOF				= 0x01,
-	TOK_EOL				= 0x02,
-	TOK_WORD			= 0x04,
-	TOK_ASGNMT			= 0x08,
-	TOK_REDIR_OP		= 0x10,
-	TOK_PIPE			= 0x20,
-	TOK_CHAIN_OP		= 0x40,
-	TOK_SUBSHELL_BOUND	= 0x80
+	TOK_EOF			= 0x01,
+	TOK_EOL			= 0x02,
+	TOK_WORD		= 0x04,
+	TOK_ASGNMT		= 0x08,
+	TOK_REDIR_OP	= 0x10,
+	TOK_PIPE		= 0x20,
+	TOK_CHAIN_OP	= 0x40,
+	TOK_SUBSH_BOUND	= 0x80
 };
 
-union u_symbol
+// This structure is for 
+typedef struct s_tok_frag
 {
-	char	unigram;
-	char	*bigram;
-};
-
-// Contexts
-typedef struct s_redir
-{
-	union u_symbol	type;
-	// We will add fields as needed
-}	t_redir;
-
-typedef struct s_chain
-{
-	union u_symbol	type;
-	// We will add fields as needed
-}	t_chain;
-
-typedef struct s_subshell
-{
-	union u_symbol	type;
-	// We will add fields as needed
-}	t_subshell;
+	char	*val;
+	size_t	len;
+	bool	xpandabl;
+}	t_tok_frag;
 
 typedef struct s_tok
 {
+	t_list		*strm;
+	size_t		frag_cnt;
 	enum e_tok	type;
-	char		*str;
-	ssize_t		len;
-	void		*cntx;
 }	t_tok;
 
-typedef struct s_lex
+enum e_lex_state
 {
-	const char	*input;
-	int			off;
-	// Other lexer state needed
-}	t_lex;
+	LEX_UNQUOTED,
+	LEX_PARTLY_QUOTED,
+	LEX_QUOTED
+};
+
+typedef struct s_lexer
+{
+	const char			*prompt;
+	size_t				off;
+	t_list				*toks;
+	t_list				*recent_tok;
+	enum e_lex_state	state;
+}	t_lexer;
+
+// init_lexer (I think there is no need for an init function a simple compound literal could do the job keeping thing in the realm of C instead of stepping into OOP ways)
+
+// eval_lex_state (This function will be handle the lexical analysis state transition this is the core of the lexer automaton)
+void	eval_lex_state(t_lexer *lexer);
+
+// identify_tok (like strtok but in the context of our lexer it will get next token each time called and also detect the end of the prompt thus there is no more token to identify)
+t_tok	*identify_tok(t_lexer *lexer);
+
+// analyse_prompt (This will be a function that will encapsulate the lexer
+// automaton and will use both eval_lex_state and identify_tok to fully identify
+// all tokens of a given prompt)
+void	analyse_prompt(t_lexer *lexer);
+
+// conclude_lex_analysis (Clean up function for a lexer)
+void	conclude_lex_analysis(t_lexer *lexer);
 
 #endif
