863b6e835af783a0003908b0265c7d569d433947
h4dh4d1y4551n3 <hadhadiyassine@gmail.com>
2024-08-24 16:00:11 +0100

    Rant

--- lexer.c
+++ lexer.c
@@ -6,7 +6,7 @@
 /*   By: yhadhadi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 01:10:06 by yhadhadi          #+#    #+#             */
-/*   Updated: 2024/08/23 04:08:17 by yhadhadi         ###   ########.fr       */
+/*   Updated: 2024/08/24 15:56:44 by yhadhadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,30 @@ t_lex_state	eval_lex_state(t_lexer *lexer);
 // identify_tok (like strtok but in the context of our lexer it will get next token each time called and also detect the end of the prompt thus there is no more token to identify)
 t_tok		*identify_tok(t_lexer *lexer);
 
-t_list	*analyse_prompt(t_lexer *lexer)
+t_list	*analyse_prompt(t_lexer *lexer, t_logger *logger)
 {
-	t_list	*tok;
+	t_tok	*tok;
+	t_list	*i;
+	t_list	*node;
 
+	i = lexer->toks;
 	while (*lexer->off)
 	{
 		// Populate toks using identify_tok
 		lexer->state = eval_lex_state(lexer);
 		tok = identify_tok(lexer);
 		if (!tok)
-			break ;
-		// Expect
-		if (!lexer->toks)
-			lexer->toks = tok;
-		lexer->recent_tok->next = tok;
+			break ; 
+		// Pre-syntax eval
+		// TODO
+		node = ft_lstnew(tok);
+		// if (!node)
+		//	return and handle error type
+		if (i)
+			i->next = node;
+		i = node;
 		// This is just a basic version this might be subdue to changes since I
-		// still ddin't decide how identify_tok will handle errors yet the
+		// still didn't decide how identify_tok will handle errors yet the
 		// interface is too simple for now
 	}
 	return (lexer->toks);
@@ -56,5 +63,67 @@ static t_lex_state	eval_lex_state(t_lexer *lexer)
 
 t_tok	*identify_tok(t_lexer *lexer)
 {
+	t_tok		*tok;
 
+	tok = (t_tok *)malloc(sizeof(t_tok));
+	if (!tok)
+		return (NULL);
+	*tok = (t_tok){};
+	if (lexer->state & (LEX_QUOTED | LEX_PARTLY_QUOTED))
+		identify_quoted_tok(tok, lexer);
+	else
+		identify_unquoted_tok(tok, lexer);
+	return (tok);
+}
+
+static void	identify_unquoted_tok(t_tok *tok, t_lexer *lexer)
+{
+	const char		*bounds[2] = {lexer->off, NULL};
+	t_lex_substate	state;
+
+	while (*lexer->off)
+	{
+		state = eval_lex_substate(lexer);
+		if (state == LEX_WHITESPACE)
+			// Skip
+		else if (state == LEX_WORD)
+			// Accumulate
+		else if (state == LEX_VAR)
+			// Skip indicator '$' accumulate identifier
+		else if (state == LEX_ASGNMT)
+			// Retrieve assignee into cntx as (char *) skip assignement operator '=' then accumulate value
+		else if (state == LEX_OPRTR)
+			// Retrieve valid monographs ignore invalid one look ahead in case
+			// of digraphs then retrieve them if valid or ignore them if not.
+			// When I say ignore I mean switch the state back to whatever
+			// following consumable char is without updating the bounds[0] wich
+			// is the token start and everything will flow back to the automaton
+		else if (state == LEX_SUBSH_BOUND)
+			// Retrieve subshell bound
+	}
+	// All the above checks should return out of the function since if we are
+	// out of the loop by mean of !*lexer->off then tok->type should be TOK_END
+	tok->type = TOK_END;
+}
+
+static void	identify_quoted_tok(t_tok *tok, t_lexer *lexer)
+{
+	// Check state and handle quoted part differently from partly quoted parts The logic is similar to unquoted but less stricted since fewer state are acknowledged 
+}
+
+static t_lex_substate	eval_lex_substate(t_lexer *lexer)
+{
+	if (!*lexer->off)
+		return (LEX_BOUND);
+	if (ft_isspace(*lexer->off))
+		return (LEX_WHITESPACE);
+	if (*lexer->off == '$')
+		return (LEX_VAR);
+	if (*lexer->off == '=')
+		return (LEX_ASGNMT);
+	if (ft_strchr("&<>|", *lexer->off))
+		return (LEX_OPRTR);
+	if (ft_strchr("()", *lexer->off))
+		return (LEX_SUBSH_BOUND);
+	return (LEX_WORD);
 }
--- lexer.h
+++ lexer.h
@@ -6,28 +6,27 @@
 /*   By: yhadhadi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 03:07:15 by yhadhadi          #+#    #+#             */
-/*   Updated: 2024/08/23 04:00:16 by yhadhadi         ###   ########.fr       */
+/*   Updated: 2024/08/24 15:11:50 by yhadhadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXER_H
 # define LEXER_H
 
-# include "minishell.h"
+# include "libft.h"
+# include "logger.h"
 
 enum e_tok
 {
-	TOK_EOF			= 0x01,
-	TOK_EOL			= 0x02,
-	TOK_WORD		= 0x04,
-	TOK_ASGNMT		= 0x08,
-	TOK_REDIR_OP	= 0x10,
-	TOK_PIPE		= 0x20,
-	TOK_CHAIN_OP	= 0x40,
-	TOK_SUBSH_BOUND	= 0x80
+	TOK_END			= 0x00,
+	TOK_WORD		= 0x01,
+	TOK_ASGNMT		= 0x02,
+	TOK_REDIR_OPRTR	= 0x04,
+	TOK_PIPE		= 0x08,
+	TOK_CHAIN_OPRTR	= 0x10,
+	TOK_SUBSH_BOUND	= 0x20
 };
 
-// This structure is for 
 typedef struct s_tok_frag
 {
 	char	*val;
@@ -37,35 +36,45 @@ typedef struct s_tok_frag
 
 typedef struct s_tok
 {
-	t_list		*strm;
-	size_t		frag_cnt;
+	t_list		*frags;
+	size_t		frags_cnt;
+	void		*cntx;
 	enum e_tok	type;
 }	t_tok;
 
 typedef enum e_lex_state
 {
-	LEX_UNQUOTED,
-	LEX_PARTLY_QUOTED,
-	LEX_QUOTED
+	LEX_UNQUOTED		= 0x00,
+	LEX_PARTLY_QUOTED	= 0x01,
+	LEX_QUOTED			= 0x02
 }	t_lex_state;
 
 typedef struct s_lexer
 {
 	const char	*off;
 	t_list		*toks;
-	t_list		*recent_tok;
 	t_lex_state	state;
 }	t_lexer;
+
+typedef enum e_lex_substate
+{
+	LEX_BOUND		= 0x00,
+	LEX_WHITESPACE	= 0x01,
+	LEX_WORD		= 0x02,
+	LEX_VAR			= 0x04,
+	LEX_ASGNMT		= 0x08,
+	LEX_OPRTR		= 0x10,
+	LEX_SUBSH_BOUND = 0X20
+}	t_lex_substate;
 
 // analyse_prompt (This will be a function that will encapsulate the lexer
 // automaton and will use both eval_lex_state and identify_tok to fully identify
 // all tokens of a given prompt)
-t_list				*analyse_prompt(t_lexer *lexer)
+t_list				*analyse_prompt(t_lexer *lexer, t_logger *logger)
 					__attribute__((nonnull(1)));
 
 // conclude_lex_analysis (Clean up function for a lexer)
 void				conclude_lex_analysis(t_lexer *lexer)
 					__attribute__((nonnull(1)));
-
 
 #endif
